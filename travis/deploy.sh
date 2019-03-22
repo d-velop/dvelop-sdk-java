014dacccbf9633daa83f6c9618c54f193b5c80df
tlanfer <tlanfer@gmail.com>
2019-03-22 08:49:51 +0100

    fix encrypted signature

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,8 +6,8 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "building for tag $TRAVIS_TAG"
 
-    openssl aes-256-cbc -K $encrypted_cf4983ae76a5_key -iv $encrypted_cf4983ae76a5_iv -in travis/signingkey.asc.enc -out travis/signingkey.asc -d
-    gpg --fast-import travis/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_0e0e4536fec9_key -iv $encrypted_0e0e4536fec9_iv -in travis/signature.asc.enc -out travis/signature.asc -d
+    gpg --fast-import travis/signature.asc
 
     mvn clean deploy --settings travis/settings.xml -DskipTests=true --batch-mode -Prelease
 
