581627ba6508cd15bd31c7a1cf791acc7e58d42b
tlanfer <tlanfer@gmail.com>
2019-03-22 12:46:50 +0100

    do not clean before deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -9,7 +9,7 @@ if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_0e0e4536fec9_key -iv $encrypted_0e0e4536fec9_iv -in travis/signature.asc.enc -out travis/signature.asc -d
     gpg --fast-import travis/signature.asc
 
-    mvn clean deploy --settings travis/settings.xml -DskipTests=true --batch-mode -Prelease
+    mvn deploy --settings travis/settings.xml -DskipTests=true --batch-mode -Prelease
 
 else
     echo "not a tagged build"
