fe15f27a82d6201e6a42c507887082bb8c7cc11d
tlanfer <tlanfer@gmail.com>
2019-03-22 08:27:04 +0100

    add code signature

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-find ./
+# fail on any error
+set -e
+
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    echo "building for tag $TRAVIS_TAG"
+
+    openssl aes-256-cbc -K $encrypted_cf4983ae76a5_key -iv $encrypted_cf4983ae76a5_iv -in travis/signingkey.asc.enc -out travis/signingkey.asc -d
+    gpg --fast-import travis/signingkey.asc
+
+    mvn clean deploy --settings travis/settings.xml -DskipTests=true --batch-mode -Prelease
+
+else
+    echo "not a tagged build"
+fi
